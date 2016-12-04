b0c6445aa816d3064f36d894c1be670bcc436c7b
jessezhang91 <jessezhang91@gmail.com>
2016-12-03 21:12:28 -0500

    make cache singlton via global

--- src/misc/cache.ts
+++ src/misc/cache.ts
@@ -3,6 +3,29 @@ import { OrmDefinition, OrmRef } from "../definitions/orm";
 
 import { DeferredMap } from "./deferred-map";
 
-export const ORM_DEFINITIONS_CACHE: DeferredMap<OrmRef, OrmDefinition<Orm>> = new DeferredMap<OrmRef, OrmDefinition<Orm>>();
-export const ORM_CLASSES_CACHE: DeferredMap<OrmRef, typeof Orm> = new DeferredMap<OrmRef, typeof Orm>();
-export const ORM_INSTANCES_CACHE: DeferredMap<OrmRef, Orm> = new DeferredMap<OrmRef, Orm>();
+// need to use global to ensure it's a singleton across the process
+// issues can occur otherwise if it is npm linked
+function getGlobalIfExists<T>(name: string, builder: () => T): T {
+	let key: string = `@lchemy/orm/${ name }`;
+	if (global[key] == null) {
+		let instance: T = builder();
+		global[key] = instance;
+	}
+	return global[key];
+}
+
+let ORM_DEFINITIONS_CACHE: DeferredMap<OrmRef, OrmDefinition<Orm>> = getGlobalIfExists("ORM_DEFINITIONS_CACHE", () => {
+	return new DeferredMap<OrmRef, OrmDefinition<Orm>>();
+});
+let ORM_CLASSES_CACHE: DeferredMap<OrmRef, typeof Orm> = getGlobalIfExists("ORM_CLASSES_CACHE", () => {
+	return new DeferredMap<OrmRef, typeof Orm>();
+});
+let ORM_INSTANCES_CACHE: DeferredMap<OrmRef, Orm> = getGlobalIfExists("ORM_INSTANCES_CACHE", () => {
+	return new DeferredMap<OrmRef, Orm>();
+});
+
+export {
+	ORM_DEFINITIONS_CACHE,
+	ORM_CLASSES_CACHE,
+	ORM_INSTANCES_CACHE
+};
